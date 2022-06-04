86dd1e5547445365f446da75bfb51902c1c9e739
sholaaaa2 <92998923+sholaaaa2@users.noreply.github.com>
2022-06-04 17:58:32 +0300

    Update HW1.2.java

--- HW1.2.java
+++ HW1.2.java
@@ -7,9 +7,9 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Введите число 4<n<16");
-		int n = sc.nextInt();
-		int fact = 1;
-		for (int i = 1; i <= n; i++) {
+		long n = sc.nextLong();
+		long fact = 1;
+		for (long i = 1; i <= n; i++) {
 			fact *= i;
 		}
 		System.out.println(n + "! = " + fact);
